6e0827c39d05f8d06751c1c833222cb0b50d0810
fuad1919 <149462644+fuad1919@users.noreply.github.com>
2023-11-28 16:51:05 +0800

    Add files via upload
    MENGENAL METHOD

--- Main.java
+++ Main.java
@@ -1,23 +1,69 @@
 package com.tutorial;
 
-class mahasiswa {
+class Mahasiswa{
+    // Data Member
     String nama;
     String nim;
     String prodi;
+    
+    // konstruktor
+    Mahasiswa(String nama, String nim, String prodi){
+        this.nama = nama;
+        this.nim = nim;
+        this.prodi = prodi;
+    }
+
+    // method tanpa return dan tanpa parameter
+    void show(){
+        System.out.println("Nama : " + this.nama);
+        System.out.println("NIM : " + this.nim);
+        System.out.println("Prodi : " + this.prodi);
+    }
+
+    // method tanpa return dengan parameter
+    void setNama(String nama, String nim, String prodi) {
+        this.nama = nama;
+        this.nim = nim;
+        this.prodi = prodi;
+    }
+
+    // method tanpa parameter dengan return
+    String getNama(){
+        return this.nama;
+    } 
+
+    String getNIM(){
+        return this.nim;
+    }
+
+    // method dengan parameter dengan return
+    String sayHi(String pesan) {
+        return pesan + " aku " + this.nama + " Mahasiswa PTI";
+    }
+
+
+
+
+     
 }
 
 public class Main {
-   public static void main(String[] args) {
-      // instansiasi objek dari class Mahasiswa
-      mahasiswa mhs1 = new mahasiswa(); // proses instansiasi
 
-      //memberi inisisasi pada objek baru
-      mhs1.nama = "sola";
-      mhs1.nim = "557858757";
-      mhs1.prodi = "teknik ";
+    public static void main(String[] args) {
+        // membuat objek baru atau instansiasi
+        Mahasiswa mhs1 = new Mahasiswa("coco", "12345678", "PTI");
+        
+        // memanggil method
+        mhs1.show();
+        mhs1.setNama("bima", "222427328", "lautan");
+        mhs1.show();
+     
+        System.out.println(mhs1.getNama());
+        System.out.println(mhs1.getNIM());
 
-      System.out.println("Nama :"+mhs1.nama);
-      System.out.println("NIM :"+mhs1.nim);
-      System.out.println("Prodi :"+mhs1.prodi);
-   } 
+        String pesan = mhs1.sayHi("Hallo");
+        System.out.println(pesan);
+
+
+    }
 }
