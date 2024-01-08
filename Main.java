612bf7ce073f72370560f68bd72b8960eda1a966
fuad1919 <149462644+fuad1919@users.noreply.github.com>
2024-01-08 10:29:00 +0800

    Add files via upload
    lkm 3 pewarisan

--- Main.java
+++ Main.java
@@ -1,51 +1,168 @@
 package com.tutorial;
 
-
-class Hero{
-    String nama;
-
-    public void display(){
-        System.out.println("nama hero : " + nama);
+class BangunDatar{
+    float Luas () {
+        return 0;
+    }
+    float Keliling () {
+        return 0;
     }
 }
 
-    //sub class hero tank
-class HeroTank extends Hero{
-        String healt;
-        String nama;
+class Segitiga extends BangunDatar{
+    float alas;
+    float tinggi;
 
-
-    public void display(){
-        System.out.println("nama hero : " + nama);
-        System.out.println("Healt : " + healt);
+    //membuat getter dan setter segitiga
+    float getalas () {
+        return alas;
     }
-}
+    
+    void setalas (float alas){
+        this.alas = alas;
+    }
 
-class HeroMarksman extends Hero{
-        String attackspeed;
-        String nama;
+    float gettingi (){
+        return tinggi;
+    }
 
-        public void display (){
-            System.out.println("nama hero 2 : " + nama);
-            System.out.println("attackspeed : " + attackspeed);
+    void settinggi (float tinggi){
+        this.tinggi = tinggi;
+    }
+    //overloading method hhitung luas
+    float hitungluas(){
+        return (float) (0.5*alas*tinggi);
+    }
+
+    //overreading method hitungkeliing 
+    float hitungKeliing(){
+        float a = (float) Math.sqrt(alas*alas)+(tinggi*tinggi);
+        return alas + tinggi + a;
         
     }
-}
 
-public class Main {
-    
-    public static void main(String[] args) {
-
-     HeroTank heroTank = new HeroTank();
-     heroTank.nama = "saitama";
-     heroTank.healt= "20";
-     heroTank.display();
-
-     HeroMarksman heroMarksman = new HeroMarksman();
-     heroMarksman.nama = "pablito";
-     heroMarksman.attackspeed = "100";
-     heroMarksman.display();
+    public void display(){
+        System.out.println("\n-----BangunDatar Segitga----- ");
+        System.out.println("luas segitiga : " + hitungluas());
+        System.out.println("keliling segitiga : " + hitungKeliing());
     }
 }
 
+    //sub class bangun datar persegi
+   class Persegi extends BangunDatar{
+        float sisi;
 
+        //membuat getter dan setter Persegi
+        float getsisi(){
+        return sisi;
+        }
+
+        void setsisi(float sisi){
+            this.sisi = sisi;
+        }
+
+        //overeading hitung luaspersegi
+        float hitungluas(){
+            return (float) (sisi*sisi);
+        }
+        //overreading hitung keliling Persegi
+        float hitungKeliing(){
+            return (float) (4*sisi);
+        }
+
+        public void display(){
+            System.out.println("\n-----BangunDatar Persegi----- ");
+            System.out.println("Luas Persegi : " + hitungluas());
+            System.out.println("Keliling Persegi : " + hitungKeliing());
+        }     
+    }
+
+    class PersegiPanjang extends BangunDatar {
+        float panjang;
+        float lebar;
+
+        //setter dan getter 
+        float getpanjang(){
+            return panjang;
+        }
+
+        void setpanjang(float panjang){
+            this.panjang = panjang;
+        }
+
+        float getlebar(){
+            return lebar;
+        }
+        
+        void setlebar(float lebar){
+            this.lebar = lebar;
+        }
+        //overeading 
+        float hitungluas(){
+            return (float) (panjang*lebar);
+        }
+
+        float hitungKeliing(){
+            return (float) (2*panjang*lebar);
+        }
+
+        public void display(){
+            System.out.println("\n--BangunDatar Persegi Panjang-- ");
+            System.out.println("Luas Persegi panjang : " + hitungluas());
+            System.out.println("Keliling PersegiPanjang : " +hitungKeliing());
+        }
+
+    }
+
+    class Lingkaran extends BangunDatar{
+    float jarijari;
+
+    //setter dan gettr
+    float getsisi(){
+        return jarijari;
+    }
+
+    void setjarijari(float jarijari){
+        this.jarijari = jarijari;
+    }
+
+    //overeading hitung luas lingkaran
+    float hitungluas(){
+        return (float) (3.14*jarijari*jarijari);
+    }
+    //overeading hitung keliling lingkaran 
+    float hitungKeliing(){
+        return (float) (2*3.14*jarijari*jarijari);
+    }
+
+    public void display(){
+        System.out.println("\n-----BangunDatar Lingkaran----- ");
+        System.out.println("Luas Liangkaran : " + hitungluas());
+        System.out.println("Kelilng Lingkaran : " + hitungKeliing());
+        System.out.println("\n");
+    }
+}
+
+ public class Main {
+ 
+    public static void main(String[] args) {
+
+        Segitiga segitiga = new Segitiga();
+        segitiga.setalas(4);
+        segitiga.settinggi(5);
+        segitiga.display();
+
+        Persegi persegi = new Persegi();
+        persegi.setsisi(4);     
+        persegi.display();
+
+        PersegiPanjang persegiPanjang = new PersegiPanjang();
+        persegiPanjang.setlebar(3);;
+        persegiPanjang.setpanjang(4);;
+        persegiPanjang.display();
+
+        Lingkaran lingkaran = new Lingkaran();
+        lingkaran.setjarijari(11);
+        lingkaran.display();
+    }
+ }
